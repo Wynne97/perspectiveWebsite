ef41891490addfadcd69973a801adb8c47110234
Shane Wynne <wynnin11@gmail.com>
2026-04-30 02:20:29 -0400

    remove firebaseConfig from .gitignore

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration for iPerspective web
+// Get these values from Firebase Console > Project Settings > General > Web app
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBr9kdbp8uiIIuqCt-MpgRwH4mH66SVVmo",
+  authDomain: "judgeme-d6d18.firebaseapp.com",
+  projectId: "judgeme-d6d18",
+  storageBucket: "judgeme-d6d18.appspot.com",
+  messagingSenderId: "927786323194",
+  appId: "1:927786323194:web:988252e3920d00994d0d43",
+};
